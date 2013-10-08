c10cc2eb7a55a5ed06283deb761df7376e6bf329
Zatvor <a.zatvornitskiy@gmail.com>
2013-10-08 18:40:26 +0300

    Run jasmine specs through grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,13 +6,20 @@ module.exports = function(grunt) {
 
     coffee: {
       compile: {
-        files: { 'dist/ember-data-elasticsearch-kit.js': [ 
-          'src/elasticsearch-adapter.coffee',
-          'src/transforms.coffee',
-          'src/query_dsl.coffee',
-          'src/mapping_dsl.coffee',
-          'src/bulk_dsl.coffee'
-        ]}
+        files: { 
+          'dist/ember-data-elasticsearch-kit.js': [ 
+            'src/elasticsearch-adapter.coffee',
+            'src/transforms.coffee',
+            'src/query_dsl.coffee',
+            'src/mapping_dsl.coffee',
+            'src/bulk_dsl.coffee'
+          ],
+
+          'spec/ember-data-elasticsearch-kit_spec.js': [
+            'spec/env.coffee',
+            'spec/*.coffee'
+          ]
+        }
       }
     },
 
@@ -23,13 +30,32 @@ module.exports = function(grunt) {
         src: 'dist/ember-data-elasticsearch-kit.js',
         dest: 'dist/ember-data-elasticsearch-kit.min.js'
       }
+    },
+
+    jasmine: {
+      pivotal: {
+        src: [
+          'vendor/assets/javascripts/jquery.js',
+          'vendor/assets/javascripts/handlebars.js',
+          'vendor/assets/javascripts/ember.js',
+          'vendor/assets/javascripts/ember-data.js',
+
+          'dist/ember-data-elasticsearch-kit.js'
+        ],
+
+        options: {
+          specs: 'spec/ember-data-elasticsearch-kit_spec.js'
+        }
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
 
   grunt.registerTask('default', ['coffee', 'uglify']);
+  grunt.registerTask('spec', ['coffee', 'jasmine:pivotal']);
 };
